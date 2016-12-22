ea4997cc5c5262c2c730d7aad758b1f724421356
Lotta Meriluoto <lottameriluoto@Lottas-MBP.home>
2016-12-21 19:56:15 -0500

    Add ch5 things

---
+++ ch5/biggerbetter.rb
@@ -0,0 +1,4 @@
+puts 'What\'s your favorite number?'
+num_int = gets.chomp.to_i
+num_string = (num_int + 1).to_s
+puts 'Well, that\'s quite nice, but isn\'t ' + num_string + ' an even bigger, better number?'
---
+++ ch5/fullname.rb
@@ -0,0 +1,7 @@
+puts 'Hiya! What\'s your first name?'
+first = gets.chomp
+puts 'And your middle name?'
+middle = gets.chomp
+puts 'And, again, your last name?'
+last = gets.chomp
+puts 'Well, hello there, ' + first + ' ' + middle + ' ' + last + '!'
