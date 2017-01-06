c8251267f9e7af37c2d8a8156f3dba66c9201483
Lotta Meriluoto <lottameriluoto@Lottas-MBP.home>
2017-01-06 15:00:23 -0500

    Complete chapter 7 'AFTtT' section

--- ch7/deafGrandmaExt.rb
+++ ch7/deafGrandmaExt.rb
@@ -3,9 +3,18 @@ while bye < 3
   greet = gets.chomp
   if greet == greet.downcase
     puts 'HUH?! SPEAK UP, SONNY!'
+    bye = 0
   elsif greet == greet.upcase && greet != 'BYE'
     puts 'NO, NOT SINCE ' + (rand(21)+1930).to_s + '!'
+    bye = 0
   elsif greet == 'BYE'
+    if bye == 0
+      puts 'RYE? I DON\'T LIKE RYE BREAD! YOU KNOW THAT!'
+    elsif bye == 1
+      puts 'LIE? GRAMMY DOESN\'T TELL LIES, SONNY. THAT\'S YOUR GRANDPA YOU MEAN.'
+    else
+            puts 'BYE, DEARIE!'
+    end
     bye = bye + 1
   end
 end
--- ch7/leapYears.rb
+++ ch7/leapYears.rb
@@ -3,9 +3,7 @@ starting = gets.chomp.to_i
 puts 'Give an ending year:'
 ending = gets.chomp.to_i
 while starting <= ending
-  if starting % 4 == 0 && starting % 100 != 0
-      puts starting
-  elsif starting % 4 == 0 && starting % 100 == 0 && starting % 400 == 0
+  if (starting % 4 == 0 && starting % 100 != 0) || (starting % 4 == 0 && starting % 100 == 0 && starting % 400 == 0)
       puts starting
   end
   starting = starting + 1
